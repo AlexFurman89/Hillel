66fdaa067a033a609f5a5e4fba1788b548480465
alexf <alexfurman20@gmail.com>
2024-02-11 22:54:30 +0200

    initial commit

---
+++ lesson_2.py
@@ -0,0 +1,13 @@
+# 1. Користувач вводить три цифри з клавіатури. Залежно від вибору користувача програма
+# виводить на екран
+# максимум із трьох, мінімум із трьох або середньоарифметичне трьох чисел.
+
+numbers = []
+while True:
+    numbers.append(int(input("Enter Number: ")))
+    if len(numbers) == 3:
+        break
+print(f"max value: {max(numbers)}")
+print(f"min value: {min(numbers)}")
+avg_value = sum(numbers)/len(numbers)
+print(f"max value: {avg_value}")
