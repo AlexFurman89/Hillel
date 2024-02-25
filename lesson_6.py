ee7b8ab5f7c3a2f5a30ca1300960f3a23fc5a6f5
AlexFurman89 <49728152+AlexFurman89@users.noreply.github.com>
2024-02-25 22:03:51 +0200

    Lesson 4

---
+++ lesson_6.py
@@ -0,0 +1,36 @@
+# Завдання:
+# множини, списки, рядки:
+#
+# 1. Створити список чисел. Заберіть дублікати значень. Вивести унікальні значення.
+# import random
+# random_list_1 = [random.randint(1,10) for i in range(5)]
+# distinct_set=set(random_list_1)
+# print(random_list_1)
+# print(distinct_set)
+# print(distinct_list:=list(distinct_set))
+
+# 2. Дано два списки чисел.
+# Порахуйте, скільки унiкальних чисел міститься як у першому списку, і у другому.
+# import random
+# random_list_1 = [random.randint(1,10) for i in range(5)]
+# random_list_2 = [random.randint(1,10) for i in range(5)]
+# #list 1
+# print(random_list_1)
+# print(set(random_list_1))
+# print(len(set(random_list_1)))
+# #list 2
+# print(random_list_2)
+# print(set(random_list_2))
+# print(len(set(random_list_2)))
+
+
+# 3. Дано два списки однакової довжини. Необхідно створити з них словник таким чином, щоб елементи першого
+# списку були ключами, а елементи другого відповідно значеннями нашого словника.
+# car_data_key=["brand","engine","price"]
+# car_data_value=["RAM TRX",6.3,100_000]
+# print(car_data_key)
+# print(car_data_value)
+# car_data_mapped={}
+# for _ in range(len(car_data_key)):
+#     car_data_mapped[car_data_key[_]]=car_data_value[_]
+# print(car_data_mapped)
